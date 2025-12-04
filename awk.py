9500dde32dad35f26e177ea67ad096910b19d9de
Your Name <you@example.com>
2025-12-04 13:21:55 +0100

    stdin_and_arg

--- awk.py
+++ awk.py
@@ -1,24 +1,30 @@
 #Input is : python awk.py intxt "$1 $2 $3"
+#Input is : python awk.py -stdin "$1 $2 $3"
 
 import sys
 
 def parseInput():
 	av = sys.argv
 	ac = len(av)
-	if ac != 2: exit()
-	inTxt = [line.split(' ') for line in sys.stdin][0]
-	inTxt = [part.replace('\n', '') for part in inTxt]
+	if ac != 3: exit()
+	if av[1] == '-stdin':
+		inTxt = [line.split(' ') for line in sys.stdin][0]
+	else:
+		inTxt = av[1].split()
 	print(inTxt)
-	idx = av[1].split(' ')
+	inTxt = [part.replace('\n', '') for part in inTxt]
+	idx = av[2].split(' ')
+	#print(inTxt)
+	#idx = av[1].split(' ')
 	print(idx)
 	idx = [i for i in idx if len(i)]
 	print(idx)
 	idx = [int(i[1:]) for i in idx if i[0] == '.']
 	print(idx)
-	
-	output = av[1]
+	output = av[2]
 	for i in idx: output = output.replace(f".{i}", inTxt[i - 1])
 	print("Final Output:", output)
+
 
 def printOutput(): pass
 def saveOutput(): pass
