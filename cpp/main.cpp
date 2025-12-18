0fad73ee15f0d4aed0ea970c0a2cac581b6118f7
Your Name <you@example.com>
2025-12-18 17:44:10 +0100

    newcpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,31 +1,40 @@
-/*
-#Input is : python awk.py intxt "$1 $2 $3"
-#Input is : python awk.py -stdin "$1 $2 $3"
-*/
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+using namespace std;
 
-#include "header.h"
-
-int	main(int ac, char **av)
+int main()
 {
-	unsigned int	i;
-	int		*idx;
-	char		**in_txt;
-	unsigned int	count_idx;
-	unsigned int	count_words;
-	std::vector<int>		idx_v;
-	std::vector<std::string>	in_txt_v;
+	int			i;
+	string			t;
+	string			inTxt;
+	string			which_tokens;
+	vector<string>		tokens;
+	vector<unsigned int>	tokenIndexes;
 
 
-	if (ac != 3)
-		return (1);
-	in_txt = get_in_txt(av);
-	count_words = ft_get_arr_lenght(in_txt);
-	idx = get_idx(av, &count_idx);
-	get_output(idx, count_idx, in_txt, count_words);
-	i = 0;
-	while (i < count_idx)
-		free(in_txt[i++]);
-	free(idx);
-	free(in_txt);
+	std::cout << "Implementing awk...\n";
+	inTxt = "11 13";
+	which_tokens = ".2 .1 .45 -1";
+
+	stringstream ss(inTxt);
+	while (getline(ss, t, ' '))
+		tokens.push_back(t);
+	stringstream ss2(which_tokens);
+	while (getline(ss2, t, ' '))
+	{
+		i = -1;
+		while (++i < t.size())
+			t[i] = t[i + 1];
+		cout << t << "\n";
+		tokenIndexes.push_back(atoi(t.c_str()) - 1);	
+	}
+	i = -1;
+	while (++i < tokenIndexes.size())
+	{
+		if (tokenIndexes[i] >= 0 && tokenIndexes[i] < tokens.size())
+			cout << tokens[tokenIndexes[i]] << "\n";
+	}
 	return (0);
 }
